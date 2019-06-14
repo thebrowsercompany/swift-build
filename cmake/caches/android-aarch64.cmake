354bbacdb1c34b17f2c1e11861066abf91d56de2
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-13 21:14:21 -0700

    build: extract the android configuration
    Extract the NDK configuration parameters into a cache file for future
    reuse.

---
+++ cmake/caches/android-aarch64.cmake
@@ -0,0 +1,22 @@
+
+set(CMAKE_SYSTEM_NAME Android CACHE STRING "")
+set(CMAKE_SYSTEM_VERSION 21 CACHE STRING "")
+
+set(CMAKE_ANDROID_API 21 CACHE STRING "")
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a CACHE STRING "")
+
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL Windows)
+  if(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r16b)
+    set(CMAKE_ANDROID_NDK C:/Microsoft/AndroidNDK64/android-ndk-r16b CACHE FILEPATH "")
+  elseif(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r15c)
+    set(CMAKE_ANDROID_NDK C:/Microsoft/AndroidNDK64/android-ndk-r15c CACHE FILEPATH "")
+  else()
+    message(FATAL_ERROR "unable to find android NDK")
+  endif()
+else()
+  message(FATAL_ERROR "${CMAKE_HOST_SYSTEM_NAME} is unsupported")
+endif()
+
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION clang CACHE STRING "")
+set(CMAKE_ANDROID_STL_TYPE c++_static CACHE STRING "")
+
