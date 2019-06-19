33c9549f0b3bbbc049bba10e182204fe6a13f02e
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-19 11:05:47 -0700

    android: try to make linking succeed

--- cmake/caches/android-aarch64-swift-flags.cmake
+++ cmake/caches/android-aarch64-swift-flags.cmake
@@ -8,5 +8,6 @@ set(CMAKE_SWIFT_LINK_FLAGS
   -tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
   -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/aarch64-linux-android-4.9/prebuilt/windows-x86_64
   -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-arm64
+  -Xclang-linker -fuse-ld=gold.exe
   CACHE STRING "")
 
