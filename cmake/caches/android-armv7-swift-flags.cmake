4ccdfd25dbca060057bffa629b768bba86d75051
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-19 22:01:09 -0700

    android: fix android armv7 toolchain name

--- cmake/caches/android-armv7-swift-flags.cmake
+++ cmake/caches/android-armv7-swift-flags.cmake
@@ -6,7 +6,7 @@ CACHE STRING "")
 set(CMAKE_SWIFT_LINK_FLAGS
 -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
 -tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
--Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/arm-linux-android-4.9/prebuilt/windows-x86_64
+-Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64
 -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-arm
 -Xclang-linker -fuse-ld=gold.exe
 CACHE STRING "")
