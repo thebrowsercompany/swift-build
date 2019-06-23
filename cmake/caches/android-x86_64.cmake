41a0639d06d7b095ded6e66b2b97775c36652836
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-23 16:38:47 -0700

    ICU: try adding android x64

---
+++ cmake/caches/android-x86_64.cmake
@@ -0,0 +1,18 @@
+
+set(CMAKE_SYSTEM_NAME "Android" CACHE STRING "")
+set(CMAKE_SYSTEM_VERSION "21" CACHE STRING "")
+
+set(CMAKE_ANDROID_API "21" CACHE STRING "")
+set(CMAKE_ANDROID_ARCH_ABI "x86_64" CACHE STRING "")
+
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL Windows)
+  if(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r16b)
+    set(CMAKE_ANDROID_NDK C:/Microsoft/AndroidNDK64/android-ndk-r16b CACHE FILEPATH "")
+  elseif(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r15c)
+    set(CMAKE_ANDROID_NDK C:/Microsoft/AndroidNDK64/android-ndk-r15c CACHE FILEPATH "")
+  endif()
+endif()
+
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION "clang" CACHE STRING "")
+set(CMAKE_ANDROID_STL_TYPE "c++_static" CACHE STRING "")
+
