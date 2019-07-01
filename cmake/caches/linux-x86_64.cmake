e780fe39b2c4ad5f81f0514b2798a0e52394870b
Saleem Abdulrasool <compnerd@compnerd.org>
2019-07-01 11:12:11 -0700

    cache: set c/c++ compiler to clang for Linux

--- cmake/caches/linux-x86_64.cmake
+++ cmake/caches/linux-x86_64.cmake
@@ -1,4 +1,7 @@
 
+set(CMAKE_C_COMPILER clang CACHE STRING "")
+set(CMAKE_CXX_COMPILER clang++ CACHE STRING "")
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-stack-protector -fomit-frame-pointer -fdata-sections -ffunction-sections" CACHE STRING "" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "-Xlinker --gc-sections" CACHE STRING "" FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS "-Xlinker --gc-sections" CACHE STRING "" FORCE)
