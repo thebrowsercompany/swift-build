d34a88097c44b4d4d56bd6bb80c28d3abaaf8e00
Saleem Abdulrasool <abdulras@fb.com>
2019-07-03 18:23:36 +0000

    Updated linux-x86_64-swift-flags.cmake
    Try to augment the swift flags for native compilation

--- cmake/caches/linux-x86_64-swift-flags.cmake
+++ cmake/caches/linux-x86_64-swift-flags.cmake
@@ -1,8 +1,10 @@
 
 set(CMAKE_SWIFT_FLAGS
       -sdk ${CMAKE_Swift_SDK}
+      -Xclang --sysroot=/
     CACHE STRING "")
 set(CMAKE_SWIFT_LINK_FLAGS
       -sdk ${CMAKE_Swift_SDK}
+      -Xclang-linker --sysroot=/
     CACHE STRING "")
 
