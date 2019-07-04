0249fe67824a317b526fe910c357a13a87e58fde
Saleem Abdulrasool <compnerd@compnerd.org>
2019-07-04 20:05:26 +0000

    Updated linux-x86_64-swift-flags.cmake
    Use `--sysroot=` as that is a driver level flag, `-isysroot /` is a frontend flag.

--- cmake/caches/linux-x86_64-swift-flags.cmake
+++ cmake/caches/linux-x86_64-swift-flags.cmake
@@ -1,7 +1,7 @@
 
 set(CMAKE_SWIFT_FLAGS
       -sdk ${CMAKE_Swift_SDK}
-      -Xcc -isysroot -Xcc /
+      -Xcc --sysroot=/
     CACHE STRING "")
 set(CMAKE_SWIFT_LINK_FLAGS
       -sdk ${CMAKE_Swift_SDK}
