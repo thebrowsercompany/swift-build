8aae5dbd1f8924b7e191c18f1b501a5b542daf74
Saleem Abdulrasool <compnerd@compnerd.org>
2019-07-10 08:03:06 -0700

    android: try to use our compiler to build the standard library

--- cmake/caches/swift-stdlib-android-x86_64.cmake
+++ cmake/caches/swift-stdlib-android-x86_64.cmake
@@ -11,8 +11,18 @@ set(SWIFT_INCLUDE_DOCS NO CACHE BOOL "")
 set(SWIFT_BUILD_RUNTIME_WITH_HOST_COMPILER YES CACHE BOOL "")
 
 # android configuration
-set(SWIFT_ANDROID_API_LEVEL ${CMAKE_ANDROID_API} CACHE STRING "")
-set(SWIFT_ANDROID_NDK_PATH ${CMAKE_ANDROID_NDK} CACHE STRING "")
+set(SWIFT_ANDROID_API_LEVEL 21 CACHE STRING "")
+if($ENV{SWIFT_ANDROID_NDK_PATH})
+  set(SWIFT_ANDROID_NDK_PATH $ENV{SWIFT_ANDROID_NDK_PATH} CACHE STRING "")
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Windows)
+  if(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r16b)
+    set(SWIFT_ANDROID_NDK_PATH "C:/Microsoft/AndroidNDK64/android-ndk-r16b" CACHE STRING "")
+  else()
+    message(FATAL_ERROR "unable to find android NDK")
+  endif()
+else()
+  message(FATAL_ERROR "unable to find android NDK")
+endif()
 set(SWIFT_ANDROID_NDK_GCC_VERSION 4.9 CACHE STRING "")
 
 # TODO(compnerd) we should fix the lld.exe spelling
