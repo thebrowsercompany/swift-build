e40aabc6fdc07e43c6f7f04031295ef3ec963773
Saleem Abdulrasool <compnerd@compnerd.org>
2019-08-02 06:42:02 +0000

    Updated android-armv7-swift-flags.cmake
    Update android armv7 swift flags

--- cmake/caches/android-armv7-swift-flags.cmake
+++ cmake/caches/android-armv7-swift-flags.cmake
@@ -1,13 +1,14 @@
 
+set(CMAKE_SWIFT_COMPILER_TARGET armv7-unknown-linux-androideabi CACHE STRING "")
 set(CMAKE_SWIFT_FLAGS
--resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
--Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot
-CACHE STRING "")
+      -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
+      -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot
+    CACHE STRING "")
 set(CMAKE_SWIFT_LINK_FLAGS
--resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
--tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
--Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64
--Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-arm
--Xclang-linker -fuse-ld=gold.exe
-CACHE STRING "")
+      -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
+      -tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
+      -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64
+      -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-arm
+      -Xclang-linker -fuse-ld=gold.exe
+    CACHE STRING "")
 
