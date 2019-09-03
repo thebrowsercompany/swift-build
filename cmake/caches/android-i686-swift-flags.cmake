6690815194452f53c577e9d96d9cedbc9a3cfe04
Saleem Abdulrasool <compnerd@compnerd.org>
2019-09-02 22:13:45 -0700

    cmake: add android i686 caches

---
+++ cmake/caches/android-i686-swift-flags.cmake
@@ -0,0 +1,15 @@
+
+set(CMAKE_SWIFT_COMPILER_TARGET i686-unknown-linux-android CACHE STRING "")
+set(CMAKE_SWIFT_FLAGS
+      -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
+      -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot
+    CACHE STRING "")
+set(CMAKE_SWIFT_LINK_FLAGS
+      -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
+      -tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
+      -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/x86_64-4.9/prebuilt/windows-x86_64
+      -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-x86
+      -Xclang-linker -fuse-ld=gold.exe
+    CACHE STRING "")
+
+
