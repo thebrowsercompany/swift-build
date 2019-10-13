947607972f59d361012668709b3d537e3bfc5430
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-13 14:03:24 -0700

    SDK: try to repair Windows dispatch build

---
+++ cmake/caches/Windows-x86_64-swift-flags.cmake
@@ -0,0 +1,4 @@
+
+set(CMAKE_Swift_COMPILER_TARGET x86_64-unknown-windows-msvc CACHE STRING "")
+set(CMAKE_Swift_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
+set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
