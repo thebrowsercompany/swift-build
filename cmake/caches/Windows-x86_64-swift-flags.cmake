0d9be18592064f48654c0de3311d30092c169462
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-13 17:17:09 -0700

    SDK: attempt to build Foundation again

--- cmake/caches/Windows-x86_64-swift-flags.cmake
+++ cmake/caches/Windows-x86_64-swift-flags.cmake
@@ -2,3 +2,5 @@
 set(CMAKE_Swift_COMPILER_TARGET x86_64-unknown-windows-msvc CACHE STRING "")
 set(CMAKE_Swift_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
 set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
+
+set(CMAKE_SWIFT_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
