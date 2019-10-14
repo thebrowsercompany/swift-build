f2ec5c69826ba1b6c5c23b2c8960433c2115d1b4
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-13 18:23:39 -0700

    build: try to build XCTest again

--- cmake/caches/Windows-x86_64-swift-flags.cmake
+++ cmake/caches/Windows-x86_64-swift-flags.cmake
@@ -4,3 +4,4 @@ set(CMAKE_Swift_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CAC
 set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
 
 set(CMAKE_SWIFT_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
+set(CMAKE_SWIFT_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
