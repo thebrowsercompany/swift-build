1635abcaad734ecd4fda1e3592cd972590ea3da8
Saleem Abdulrasool <compnerd@compnerd.org>
2019-11-02 22:08:02 -0700

    android: add some more flags to CMAKE_Swift_FLAGS

--- cmake/caches/android-armv7-swift-flags.cmake
+++ cmake/caches/android-armv7-swift-flags.cmake
@@ -16,5 +16,5 @@ if(CMAKE_VERSION VERSION_LESS 3.16)
   list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CMAKE_Swift_COMPILER_TARGET)
 endif()
 set(CMAKE_Swift_COMPILER_TARGET armv7-unknown-linux-androideabi CACHE STRING "")
-set(CMAKE_Swift_FLAGS "-resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot" CACHE STRING "")
+set(CMAKE_Swift_FLAGS "-resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot -Xclang-linker -fuse-ld=lld -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-arm" CACHE STRING "")
 
