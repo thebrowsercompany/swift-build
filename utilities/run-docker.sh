01541ee9b347ba2060e0cf08d7065dd79eb9f058
Saleem Abdulrasool <compnerd@compnerd.org>
2019-11-18 19:35:13 -0800

    utilities: add helper to launch docker

---
+++ utilities/run-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p ${HOME}/Source
+mkdir -p ${HOME}/Library/Developer/org.compnerd.dt/DerivedData
+mkdir -p ${HOME}/.config/Code
+sudo docker run --detach --publish 0.0.0.0:8080:8080 --cap-add SYS_PTRACE --security-opt seccomp=unconfined --volume ${HOME}/Source:/SourceCache --volume ${HOME}/Library/Developer/org.compnerd.dt/DerivedData:/BinaryCache --volume "${HOME}/.config/Code:/Library/Application Support/com.coder.code-server" compnerd/swift
