38f2073be7d394ec3a8744755264289e52d7edab
Saleem Abdulrasool <compnerd@compnerd.org>
2019-11-18 22:57:00 -0800

    docker: further refinement

--- utilities/run-docker.sh
+++ utilities/run-docker.sh
@@ -2,4 +2,4 @@
 mkdir -p ${HOME}/Source
 mkdir -p ${HOME}/Library/Developer/org.compnerd.dt/DerivedData
 mkdir -p ${HOME}/.config/Code
-sudo docker run --detach --publish 0.0.0.0:8080:8080 --cap-add SYS_PTRACE --security-opt seccomp=unconfined --volume ${HOME}/Source:/SourceCache --volume ${HOME}/Library/Developer/org.compnerd.dt/DerivedData:/BinaryCache --volume "${HOME}/.config/Code:/Library/Application Support/com.coder.code-server" compnerd/swift
+sudo docker run --detach --publish 0.0.0.0:8080:8080 --cap-add SYS_PTRACE --security-opt seccomp=unconfined --volume ${HOME}/Source:/SourceCache --volume ${HOME}/Library/Developer/org.compnerd.dt/DerivedData:/BinaryCache --volume "${HOME}/.config/Code:/Users/Shared/Library/Application Support/com.coder.code-server" compnerd/swift
