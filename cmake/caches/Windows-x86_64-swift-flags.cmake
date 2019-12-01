d847d6e2189dd8cac01eb13624387f86de1fdd92
Saleem Abdulrasool <compnerd@compnerd.org>
2019-12-01 10:43:17 -0800

    caches: explicitly add link path for Swift :-(

--- cmake/caches/Windows-x86_64-swift-flags.cmake
+++ cmake/caches/Windows-x86_64-swift-flags.cmake
@@ -1,7 +1,7 @@
 
 set(CMAKE_Swift_COMPILER_TARGET x86_64-unknown-windows-msvc CACHE STRING "")
-set(CMAKE_Swift_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
-set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
+set(CMAKE_Swift_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\" -L${SWIFT_WINDOWS_SDK}/usr/lib/swift/windows" CACHE STRING "")
+set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\" -L${SWIFT_WINDOWS_SDK}/usr/lib/swift/windows" CACHE STRING "")
 
 set(CMAKE_SWIFT_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
 set(CMAKE_SWIFT_LINK_FLAGS "-resource-dir \"${SWIFT_WINDOWS_SDK}/usr/lib/swift\"" CACHE STRING "")
