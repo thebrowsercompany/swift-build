57e2c550d253c426833b7b6fb71f624b742e7178
Saleem Abdulrasool <compnerd@compnerd.org>
2020-01-01 11:55:53 -0800

    android: rename SWIFT_ANDROID_SDK to CMAKE_Swift_SDK

--- cmake/caches/android-i686-swift-flags.cmake
+++ cmake/caches/android-i686-swift-flags.cmake
@@ -1,11 +1,11 @@
 
 set(CMAKE_SWIFT_COMPILER_TARGET i686-unknown-linux-android CACHE STRING "")
 set(CMAKE_SWIFT_FLAGS
-      -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
+      -resource-dir ${CMAKE_Swift_SDK}/usr/lib/swift
       -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot
     CACHE STRING "")
 set(CMAKE_SWIFT_LINK_FLAGS
-      -resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift
+      -resource-dir ${CMAKE_Swift_SDK}/usr/lib/swift
       -tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
       -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/x86_64-4.9/prebuilt/windows-x86_64
       -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-x86
@@ -16,5 +16,5 @@ if(CMAKE_VERSION VERSION_LESS 3.16)
   list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CMAKE_Swift_COMPILER_TARGET)
 endif()
 set(CMAKE_Swift_COMPILER_TARGET i686-unknown-linux-android CACHE STRING "")
-set(CMAKE_Swift_FLAGS "-resource-dir ${SWIFT_ANDROID_SDK}/usr/lib/swift -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/x86_64-4.9/prebuilt/windows-x86_64 -Xclang-linker -fuse-ld=gold -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-x86" CACHE STRING "")
+set(CMAKE_Swift_FLAGS "-resource-dir ${CMAKE_Swift_SDK}/usr/lib/swift -Xcc --sysroot=${CMAKE_ANDROID_NDK}/sysroot -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/x86_64-4.9/prebuilt/windows-x86_64 -Xclang-linker -fuse-ld=gold -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-x86" CACHE STRING "")
 
