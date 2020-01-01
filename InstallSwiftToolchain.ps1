9c9eb919eb025d11368c105e47503cb72472d4fa
Saleem Abdulrasool <compnerd@compnerd.org>
2020-01-01 15:36:27 -0800

    powershell: remove scripts which no longer function

--- InstallSwiftToolchain.ps1
+++
@@ -1,8 +0,0 @@
-
-$ToolchainBuild = Invoke-RestMethod -Uri 'https://dev.azure.com/compnerd/windows-swift/_apis/build/builds?definitions=22&resultFilter=succeeded,partiallySucceeded&$top=1&api-version-string=5.0' -Method GET -UseDefaultCredentials
-$ToolchainBuildID = $ToolchainBuild.value.id
-$ToolchainArtifacts = Invoke-RestMethod -Uri "https://dev.azure.com/compnerd/windows-swift/_apis/build/builds/$ToolchainBuildID/artifacts?apiversion-string=2.0" -Method GET -UseDefaultCredentials
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest $ToolchainArtifacts.value[0].resource.downloadUrl -OutFile "${env:temp}\$($ToolchainArtifacts.value[0].name)" -UseBasicParsing
-Start-Process "${env:temp}\$($ToolchainArtifacts.value[0].name)" -ArgumentList "/qn" -Wait
-# Remove-Item "${env:temp}\$($ToolchainArtifacts.value[0].name)" -Force
