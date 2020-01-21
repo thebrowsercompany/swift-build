6e98fcf5b7f183572eb7da41766f50114220e408
Alexander Smarus <bender@readdle.com>
2020-01-21 22:15:19 +0200

    Publish non-test SDK components on Windows

--- cmake/caches/windows-x86_64-swift-flags.cmake
+++ cmake/caches/windows-x86_64-swift-flags.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_Swift_COMPILER_TARGET x86_64-unknown-windows-msvc CACHE STRING "")
-set(CMAKE_Swift_FLAGS "-resource-dir \"${CMAKE_Swift_SDK}/usr/lib/swift\" -L${CMAKE_Swift_SDK}/usr/lib/swift/windows" CACHE STRING "")
-set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${CMAKE_Swift_SDK}/usr/lib/swift\" -L${CMAKE_Swift_SDK}/usr/lib/swift/windows" CACHE STRING "")
 
-set(CMAKE_SWIFT_FLAGS "-resource-dir \"${CMAKE_Swift_SDK}/usr/lib/swift\"" CACHE STRING "")
-set(CMAKE_SWIFT_LINK_FLAGS "-resource-dir \"${CMAKE_Swift_SDK}/usr/lib/swift\"" CACHE STRING "")
+set(SWIFT_STDLIB_DIR "${CMAKE_Swift_SDK}/usr" CACHE STRING "")
+
+set(CMAKE_Swift_FLAGS "-resource-dir \"${SWIFT_STDLIB_DIR}/lib/swift\" -L${SWIFT_STDLIB_DIR}/lib/swift/windows" CACHE STRING "")
+set(CMAKE_Swift_LINK_FLAGS "-resource-dir \"${SWIFT_STDLIB_DIR}/lib/swift\" -L${SWIFT_STDLIB_DIR}/lib/swift/windows" CACHE STRING "")
 
 if(CMAKE_VERSION VERSION_LESS 3.16.0)
   set(CMAKE_Swift_LINK_LIBRARY_FLAG "-l" CACHE STRING "")
