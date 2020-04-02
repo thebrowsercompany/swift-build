72a0862d3080b3b6a181d9c7bfd1b082c94cf7b6
Saleem Abdulrasool <compnerd@compnerd.org>
2020-04-02 10:05:42 -0700

    CI: attempt to repair macOS build

--- cmake/caches/darwin-x86_64.cmake
+++ cmake/caches/darwin-x86_64.cmake
@@ -5,3 +5,6 @@ set(CMAKE_CXX_COMPILER clang++ CACHE STRING "")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-stack-protector -fomit-frame-pointer" CACHE STRING "" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "" CACHE STRING "" FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS "" CACHE STRING "" FORCE)
+
+# TODO(compnerd) figure out why the scripts and the framework cannot co-install
+set(SWIFT_BUILD_DISABLE_LLDB_PYTHON_SCRIPTS TRUE CACHE BOOL "" FORCE)
