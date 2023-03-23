9067435e08e5d02d6733ec626a8c69317b714d69
Saleem Abdulrasool <abdulras@thebrowser.company>
2023-03-23 09:08:11 -0700

    build: add a workaround for CMake 3.26+
    CMake 3.26 enables `-wmo` on release mode builds.  This does not work
    properly with the C++ driver which will attempt to read the import or
    static libraries as Swift source code, which fails.  Temporarily
    workround this by disabling WMO.

--- build.ps1
+++ build.ps1
@@ -307,6 +307,9 @@ function Build-CMakeProject
 
     $SwiftcFlags = $SwiftArgs.ToArray() -Join " "
     Append-FlagsDefine $Defines CMAKE_Swift_FLAGS $SwiftcFlags
+
+    # Workaround CMake 3.26+ enabling `-wmo` by default on release builds
+    Append-FlagsDefine $Defines CMAKE_Swift_FLAGS_RELEASE "-O"
   }
   if ("" -ne $InstallTo) {
     TryAdd-KeyValue $Defines CMAKE_INSTALL_PREFIX $InstallTo
