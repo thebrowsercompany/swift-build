3c9c4c64a30c64a11758db7fbe4108cfb7074a34
Hiroshi Yamauchi <56735936+hjyamauchi@users.noreply.github.com>
2023-06-15 08:54:48 -0700

    Turn off safeseh for foundation on X86. (#567)
    Because lld has safeseh enabled by default and fails with a special
    icu data object file icudt69l_dat.obj.
    
    Co-authored-by: Saleem Abdulrasool <compnerd@compnerd.org>

--- build.ps1
+++ build.ps1
@@ -809,6 +809,10 @@ function Build-Foundation($Arch, [switch]$Test = $false) {
         CMAKE_INSTALL_PREFIX = "$($Arch.SDKInstallRoot)\usr";
         CMAKE_SYSTEM_NAME = "Windows";
         CMAKE_SYSTEM_PROCESSOR = $Arch.CMakeName;
+        # Turn off safeseh for lld as it has safeseh enabled by default
+        # and fails with an ICU data object file icudt69l_dat.obj. This
+        # matters to X86 only.
+        CMAKE_Swift_FLAGS = if ($Arch -eq $ArchX86) { "-Xlinker /SAFESEH:NO" } else { "" };
         CURL_DIR = "$LibraryRoot\curl-7.77.0\usr\lib\$ShortArch\cmake\CURL";
         ICU_DATA_LIBRARY_RELEASE = "$LibraryRoot\icu-69.1\usr\lib\$ShortArch\sicudt69.lib";
         ICU_I18N_LIBRARY_RELEASE = "$LibraryRoot\icu-69.1\usr\lib\$ShortArch\sicuin69.lib";
