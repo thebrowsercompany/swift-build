e3035906295179c0ca13d90758118deb4e9449da
Saleem Abdulrasool <abdulras@thebrowser.company>
2023-08-03 10:53:36 -0700

    build: deploy CxxShim module on Windows
    The installer will copy this module as well, but we failed to deploy it
    from the staging to the local installation which prevents the local
    deployment from being used for testing C++ interop.

--- build.ps1
+++ build.ps1
@@ -1015,6 +1015,11 @@ function Install-Platform($Arch) {
     }
   }
 
+  # Copy the CxxShim module
+  foreach ($Source in ("libcxxshim.h", "libcxxshim.modulemap", "libcxxstdlibshim.h")) {
+    Copy-File "$WindowsLibSrc\$Source" "$WindowsLibDst"
+  }
+
   # Copy plist files (same across architectures)
   Copy-File "$($Arch.PlatformInstallRoot)\Info.plist" $PlatformInstallRoot\
   Copy-File "$($Arch.SDKInstallRoot)\SDKSettings.plist" $SDKInstallRoot\
